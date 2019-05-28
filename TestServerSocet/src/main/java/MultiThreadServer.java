5ebec0bd8b97c4c29b4b46fbdf0f2db84d32882e
Dmit1995Kit <dmitrij19.yakovlev95@gmail.com>
2019-05-28 17:56:29 +0300

    Test server and customer

---
+++ TestServerSocet/src/main/java/MultiThreadServer.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MultiThreadServer {
+    static ExecutorService executeIt = Executors.newFixedThreadPool(2);
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        try (ServerSocket server = new ServerSocket(3345)) {
+            System.out.println("Server socket created, " +
+                    "command console reader for listen to server commands");
+            while (!server.isClosed()) {
+                Socket client = server.accept();
+                executeIt.execute(new MonoThreadClientHandler(client));
+                System.out.print("Connection accepted.");
+            }
+            executeIt.shutdown();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
